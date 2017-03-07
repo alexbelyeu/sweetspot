a8f5a74f0984844f825f7807722ab45bd97e9a7a
Daniel Abad <abadabanades@gmail.com>
2017-03-07 09:28:56 +0100

    removed console.log

--- src/reducers/LandingReducer.js
+++ src/reducers/LandingReducer.js
@@ -14,7 +14,6 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case CHANGE_LANDING_TAB:
-      console.log(action);
       return { ...state, index: action.payload };
     case REHYDRATE: {
       const incoming = action.payload.spots;
