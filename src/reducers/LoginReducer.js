78af874c29ffd8664e7a3a27e13ed809a848ed65
Alex Belyeu <abelyeun@gmail.com>
2017-04-16 12:00:38 -0400

    Login/Register correctness

--- src/reducers/LoginReducer.js
+++ src/reducers/LoginReducer.js
@@ -3,6 +3,7 @@ import {
   USERNAME_CHANGED,
   PASSWORD_CHANGED,
   LOGIN_USER_SUCCESS,
+  REGISTER_USER_SUCCESS,
   LOGIN_USER_USERNAME_FAIL,
   LOGIN_USER_PASSWORD_FAIL,
   LOGIN_USER_ERROR,
@@ -27,13 +28,15 @@ export default (state = INITIAL_STATE, action) => {
     case LOGIN_USER:
       return { ...state, loading: true, error: '', usernameError: '', passwordError: '' };
     case LOGIN_USER_SUCCESS:
-      return { ...state, ...INITIAL_STATE, password: '' };
+      return { ...state, password: '', loading: false };
     case LOGIN_USER_USERNAME_FAIL:
       return { ...state, usernameError: action.payload, password: '', loading: false };
     case LOGIN_USER_PASSWORD_FAIL:
       return { ...state, passwordError: action.payload, password: '', loading: false };
     case LOGIN_USER_ERROR:
       return { ...state, error: action.payload, password: '', loading: false };
+    case REGISTER_USER_SUCCESS:
+      return { ...state, ...INITIAL_STATE };
 
     case REHYDRATE: {
       const incoming = action.payload.login;
--- src/reducers/RegisterReducer.js
+++ src/reducers/RegisterReducer.js
@@ -4,6 +4,7 @@ import {
   EMAIL_CREATED,
   PASSWORD_CREATED,
   REGISTER_USER_SUCCESS,
+  LOGIN_USER_SUCCESS,
   REGISTER_USER_USERNAME_FAIL,
   REGISTER_USER_EMAIL_FAIL,
   REGISTER_USER_PASSWORD_FAIL,
@@ -31,17 +32,20 @@ export default (state = INITIAL_STATE, action) => {
     case PASSWORD_CREATED:
       return { ...state, password: action.payload };
     case REGISTER_USER:
-      return { ...state, loading: true, error: '', usernameError: '', emailError: '' };
+      return { ...state, loading: true, usernameError: '', passwordError: '', emailError: '', error: '' };
     case REGISTER_USER_SUCCESS:
-      return { ...state, ...INITIAL_STATE, password: '' };
+      return { ...state, usernameError: '', emailError: '', password: '', passwordError: '', error: '', loading: false };
     case REGISTER_USER_USERNAME_FAIL:
-      return { ...state, usernameError: action.payload, password: '', passwordError: '', loading: false };
+      return { ...state, usernameError: action.payload, loading: false };
     case REGISTER_USER_EMAIL_FAIL:
-      return { ...state, emailError: action.payload, password: '', loading: false };
+      return { ...state, emailError: action.payload, loading: false };
     case REGISTER_USER_PASSWORD_FAIL:
-      return { ...state, passwordError: action.payload, password: '', loading: false };
+      return { ...state, passwordError: action.payload, loading: false };
     case REGISTER_USER_ERROR:
       return { ...state, error: action.payload, password: '', loading: false };
+    case LOGIN_USER_SUCCESS:
+      return { ...state, ...INITIAL_STATE };
+
     case REHYDRATE: {
       const incoming = action.payload.register;
       if (incoming) {
